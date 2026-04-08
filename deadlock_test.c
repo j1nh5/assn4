d0d2bc324bdb0648d72a29e3236a04662403536a
Kim Jin Ho <j1nh5@naver.com>
2026-04-08 10:56:48 +0900

    Update deadlock_test.c

--- deadlock_test.c
+++ deadlock_test.c
@@ -7,48 +7,48 @@ pthread_mutex_t mutex_a = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_b = PTHREAD_MUTEX_INITIALIZER;
 
 void* thread1_start(void* arg) {
-    printf("[Thread1] mutex_a 요구 중...\n");
+    printf("[Thread1] Request mutex_a...\n");
     pthread_mutex_lock(&mutex_a);
-    printf("[Thread1] mutex_a 점유 성공!\n");
+    printf("[Thread1] Success mutex_a!\n");
 
     sleep(1); 
 
-    printf("[Thread1] mutex_b 요구 중...\n");
+    printf("[Thread1] Request mutex_b...\n");
     pthread_mutex_lock(&mutex_b);
-    printf("[Thread1] mutex_b 점유 성공!\n");
+    printf("[Thread1] Success mutex_b!\n");
     pthread_mutex_unlock(&mutex_b);
     pthread_mutex_unlock(&mutex_a);
-    printf("[Thread1] 작업 완료!\n");
+    printf("[Thread1] Complete!\n");
     
     return NULL;
 }
 
 void* thread2_start(void* arg) {
-    printf("[Thread2] mutex_b 요구 중...\n");
+    printf("[Thread2] Request mutex_b...\n");
     pthread_mutex_lock(&mutex_b);
-    printf("[Thread2] mutex_b 점유 성공!\n");
+    printf("[Thread2] Success mutex_b!\n");
 
     sleep(1); 
 
-    printf("[Thread2] mutex_a 요구 중...\n");
+    printf("[Thread2] Request mutex_a...\n");
     pthread_mutex_lock(&mutex_a);
-    printf("[Thread2] mutex_a 점유 성공!\n");
+    printf("[Thread2] Success mutex_a!\n");
     pthread_mutex_unlock(&mutex_a);
     pthread_mutex_unlock(&mutex_b);
-    printf("[Thread2] 작업 완료!\n");
+    printf("[Thread2] Complete!\n");
     
     return NULL;
 }
 
 int main() {
     pthread_t thread1, thread2;
-    printf("--- 프로그램 시작 ---\n");
+    printf("--- Program Start ---\n");
     
     pthread_create(&thread1, NULL, thread1_start, NULL);
     pthread_create(&thread2, NULL, thread2_start, NULL);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
-    printf("--- 프로그램 정상 종료 ---\n");
+    printf("--- Program End ---\n");
     return 0;
 }
