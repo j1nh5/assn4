ccae0bed7af10ae03e0ab2e9f6d08b2a65432577
Kim Jin Ho <j1nh5@naver.com>
2026-04-08 10:59:17 +0900

    Update deadlock2.c

--- deadlock2.c
+++ deadlock2.c
@@ -7,13 +7,13 @@ pthread_mutex_t mutex_a = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_b = PTHREAD_MUTEX_INITIALIZER;
 
 void* thread1_start(void* arg) {
-    printf("[Thread1] mutex_a 요구 중...\n");
+    printf("[Thread1] Request mutex_a...\n");
     pthread_mutex_lock(&mutex_a);
     printf("[Thread1] Success mutex_a!\n");
 
     sleep(1); 
 
-    printf("[Thread1] mutex_b 요구 중...\n");
+    printf("[Thread1] Request mutex_b\n");
     pthread_mutex_lock(&mutex_b);
     printf("[Thread1] Success mutex_b!\n");
     pthread_mutex_unlock(&mutex_b);
@@ -24,13 +24,13 @@ void* thread1_start(void* arg) {
 }
 
 void* thread2_start(void* arg) {
-    printf("[Thread2] mutex_a 요구 중...\n");
+    printf("[Thread2] Request mutex_a...\n");
     pthread_mutex_lock(&mutex_a);
     printf("[Thread2] Success mutex_a!\n");
 
     sleep(1); 
 
-    printf("[Thread2] mutex_b 요구 중...\n");
+    printf("[Thread2] Request mutex_b...\n");
     pthread_mutex_lock(&mutex_b);
     printf("[Thread2] Success mutex_b!\n");
     pthread_mutex_unlock(&mutex_b);
